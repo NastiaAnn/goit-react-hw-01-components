95ed6cbf73c73175d0797a2504031fa8e6978aae
NastiaAnn <anastasianazar4@gmail.com>
2023-05-02 13:34:11 +0300

    2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
-import { Profile } from './Profile/Profile';
-import user from '../data/user.json';
+// import { Profile } from './Profile/Profile';
+// import user from '../data/user.json';
 
 export const App = () => {
   return (
@@ -13,13 +13,13 @@ export const App = () => {
         color: '#010101',
       }}
     >
-      <Profile
+      {/* <Profile
         username={user.username}
         tag={user.tag}
         location={user.location}
         avatar={user.avatar}
         stats={user.stats}
-      />
+      /> */}
     </div>
   );
 };
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,3 +1,28 @@
-export const Statistics = () => {
-  return;
+import css from './Statistics.module.css';
+
+export const StatisticBoard = ({ title, id, label, percentage }) => {
+  return (
+    <section className={css.statistics}>
+      <h2 className={css.title}>{title}</h2>
+
+      <ul className={css.statList}>
+        <li className={css.item}>
+          <span className={css.label}>{label}</span>
+          <span className={css.percentage}>{percentage}</span>
+        </li>
+        <li className={css.item}>
+          <span className={css.label}>{label}</span>
+          <span className={css.percentage}>{percentage}</span>
+        </li>
+        <li className={css.item}>
+          <span className={css.label}>{label}</span>
+          <span className={css.percentage}>{percentage}</span>
+        </li>
+        <li className={css.item}>
+          <span className={css.label}>{label}</span>
+          <span className={css.percentage}>{percentage}</span>
+        </li>
+      </ul>
+    </section>
+  );
 };
